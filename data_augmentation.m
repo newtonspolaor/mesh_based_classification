9399640d530726339e8c71ac068c8645b4d3e7e5
newtonspolaor <newtonspolaor@gmail.com>
2024-12-04 15:27:56 -0300

    Update data_augmentation.m

--- data_augmentation.m
+++ data_augmentation.m
@@ -1,6 +1,9 @@
 function [img_r90,img_r180,img_s150_50,img_s80_120]=data_augmentation(img)
-%Function that applies rotation and resizing procedures on each dermoscopy examination to derive four other images, as described in the manuscript associated with this repository
+%Function that applies rotation and resizing procedures on each dermoscopy examination received as input to return four other images, as described in the manuscript associated with this repository
+%Requires: an image img pre-processed as described in the manuscript
+%Ensures: four images that represent variations from the input image according to a specific rotation and/or resize procedure
 
+%Please refer to the Matlab documentation to verify requirements and more information on the built-in functions imrotate and imresize
 img_r90 = imrotate(img,-90,'bilinear','loose');
 img_r180 = imrotate(img,-180,'bilinear','loose');
 img_s150_50 = imresize(img,[1.50*size(img,1) 0.50*size(img,2)]);
